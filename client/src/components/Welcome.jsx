ddf77ccf17a1934d28e356c27a24b7fed74f7a1f
Alvi Hasan <livealvi@gmail.com>
2022-06-03 04:57:06 +0600

    add: useContext

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 import { Loader } from "./";
+import { TransactionContext } from "../context/TransactionContext";
 
 const commonStyle =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-white";
@@ -19,6 +20,8 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
+  const { value } = useContext(TransactionContext);
+  console.log(value);
   const connectWallet = () => {};
   const handelSubmit = () => {};
 
